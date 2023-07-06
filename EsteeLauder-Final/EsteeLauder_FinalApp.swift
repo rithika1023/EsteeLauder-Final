a408d8b3ed6cacba2c16a1c76dd6cb7744d22660
Rithika Manchala <manchalarithika@gmail.com>
2023-07-05 20:33:17 -0500

    Initial Commit

---
+++ EsteeLauder-Final/EsteeLauder_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsteeLauder_FinalApp.swift
+//  EsteeLauder-Final
+//
+//  Created by Ravi Manchala on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct EsteeLauder_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
